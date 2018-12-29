26b0dd2c476bbc03b1e459e387d536c03d27bce4
sameerMalik786 <muhammadshuaib@yahoo.in>
2018-12-29 21:47:22 +0530

    pupteer added to click and increase image count from 12 to 35 for flipkart

--- server/helpers/scraper.js
+++ server/helpers/scraper.js
@@ -1,14 +1,29 @@
+const puppeteer = require("puppeteer");
 const rp = require("request-promise");
 const $ = require("cheerio");
 
 module.exports = async function(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.click("._1pnbvM");
+  await page.waitFor(500);
+  const content = await page.content();
+  const urls = await scrapeUrls(content);
+  console.log(urls);
+  // await browser.close();
+  return urls;
+};
+
+async function scrapeUrls(html) {
   try {
     let images = [];
-    const html = await rp(url);
-    console.log($("._3rw8Cw", html).length);
-    for (let i = 0; i < $("._3rw8Cw", html).length; i++) {
+    console.log($("._2xsyJg", html).length);
+    for (let i = 0; i < $("._2xsyJg", html).length; i++) {
       var pattern = new RegExp("https://rukminim1" + "(.*)" + ".jpg");
-      let [ imageUrl, ...something ] = pattern.exec($("._3rw8Cw", html)[i].attribs.style);
+      let [imageUrl, ...something] = pattern.exec(
+        $("._2xsyJg", html)[i].attribs.style
+      );
       images.push(imageUrl);
     }
     return images;
@@ -16,4 +31,4 @@ module.exports = async function(url) {
     //handle error
     return "scrapper error";
   }
-};
+}
